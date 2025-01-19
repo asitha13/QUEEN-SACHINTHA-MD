a1a830a98b1f48a30895834dd8ef92734365e71c
asitha13 <botlakidu@gmail.com>
2025-01-19 19:26:52 +0530

    Create Video .js

---
+++ plugins/Video .js
@@ -0,0 +1,85 @@
+const { cmd, commands } = require('../command');
+const { fetchJson } = require('../lib/functions');
+const domain = `https://manu-ofc-api-site-6bfcbe0e18f6.herokuapp.com`;
+const api_key = `MY-VIDEO-OFC`;
+const yts = require('yt-search');
+    cmd({
+    pattern: "video",
+    desc: 'Download Song / Video',
+    use: '.play Title',
+    react: "🎬",
+    category: 'download',
+    filename: __filename
+},
+async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+ 
+    try {
+        if (!q) return reply('Please provide a title.');
+
+        const search = await yts(q);
+        const data = search.videos[0];
+        const url = data.url;
+
+        let desc = `*┈━═☆🐉 ISHAN 𝐕𝐈𝐃𝐄𝐎 🐉☆═━┈*
+> ╭────────────────◉◉➤         
+> *\`➤ Title\` :* ${data.title}
+
+> *\`➤ Views\` :* ${data.views}
+
+> *\`➤ DESCRIPTION\`:* ${data.description}
+
+> *\`➤ TIME\`:* ${data.timestamp}
+
+> *\`➤ AGO\`:* ${data.ago}
+> ╰─────────────────◉◉➤
+*◄❪ 🔢 ʀᴇᴘʟʏ ʙᴇʟᴏᴡ ᴛʜᴇ ɴᴜᴍʙᴇʀ ᴛᴏ ❫►*
+> ╭────────────────────◉◉➤
+> |➢ 1  || ᴠɪᴅᴇᴏ🎬
+> |
+> ➢  2  || ᴅᴏᴄᴜᴍᴇɴᴛ📁
+> ╰────────────────────◉◉➤
+
+> *⚜️𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 - : 𝐌𝐑 .©ISHAN 𝐎𝐅𝐂 💛*`;
+
+        const vv = await conn.sendMessage(from, { image: { url: data.thumbnail }, caption: desc }, { quoted: mek });
+
+        conn.ev.on('messages.upsert', async (msgUpdate) => {
+            const msg = msgUpdate.messages[0];
+            if (!msg.message || !msg.message.extendedTextMessage) return;
+
+            const selectedOption = msg.message.extendedTextMessage.text.trim();
+
+            if (msg.message.extendedTextMessage.contextInfo && msg.message.extendedTextMessage.contextInfo.stanzaId === vv.key.id) {
+                switch (selectedOption) {
+                    case '1':
+
+    const response = await fetchJson(`${domain}/ytmp4-dl?apikey=${api_key}&videoUrl=${url}&resolution=360`);
+    
+    const downloadUrl = response.data.dl_link;
+
+
+await conn.sendMessage(from,{video:{url: downloadUrl },mimetype:"video/mp4",caption :"> *⚜️𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 - : 𝐌𝐑 .©ISHAN 𝐎𝐅𝐂 💛*"},{quoted:mek})
+                        break;
+       
+                    case '2':               
+                        // Send Document File
+    const responsex = await fetchJson(`${domain}/ytmp4-dl?apikey=${api_key}&videoUrl=${url}&resolution=360`);
+    
+    const downloadUrlx = responsex.data.dl_link;
+
+await conn.sendMessage(from,{document:{url: downloadUrlx },mimetype:"video/mp4",fileName: data.title + ".mp4",caption :"> *⚜️𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 - : 𝐌𝐑 .©ISHAN 𝐎𝐅𝐂 💛*"},{quoted:mek})
+                        break;
+ 
+                    default:
+                        reply("Invalid option. Please select a valid option 💗");
+                }
+
+            }
+        });
+
+    } catch (e) {
+        console.error(e);
+        await conn.sendMessage(from, { react: { text: '❌', key: mek.key } })
+        reply('An error occurred while processing your request.');
+    }
+});
